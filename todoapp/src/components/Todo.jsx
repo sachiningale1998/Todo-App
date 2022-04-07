4acb16d35d62a5e7fa732d4536f3e5655424c3e1
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-04-08 01:35:06 +0530

    Todo Final

--- todoapp/src/components/Todo.jsx
+++ todoapp/src/components/Todo.jsx
@@ -3,17 +3,28 @@ import React from 'react';
 
 function Todo(){
 const [task,setTask] = React.useState("")
+const [todo,setTodo] = React.useState([]);
+const handleChange = (e)=>{
+
+    setTask(e.target.value);
+   // console.log("changing input", e.target.value) ;
+}
     const handleTask = ()=>{
+        setTodo([...todo,task])
        // setTask(task)
         console.log(task)
     }
 
     return (
         <>
-        
-        <input placeholder="Enter task" />
-        <button onClick={handleTask()}>Add Task</button>
-        
+        <h1>Todo</h1>
+        <input type={"text"} value={task} placeholder="Enter task" onChange={handleChange}/>
+        <button onClick={handleTask}>Add Task</button>
+        {
+            todo.map((item)=>{
+                return <h1>{item}</h1>
+            })
+        }
         </>
     )
 
